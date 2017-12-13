864752dfb2340a0004f9af1c6da7186d4da20a44
wabywang(王本友) <wabywang@tencent.com>
2017-12-13 17:34:53 +0800

    amore clear

--- main.py
+++ main.py
@@ -20,50 +20,22 @@ import torch.nn.functional as F
 from torchtext import data
 from torchtext import datasets
 from torchtext.vocab import Vectors, GloVe, CharNGram, FastText
+from torch.nn.modules.loss import NLLLoss
+
 opt = opts.parse_opt()
 opt.model ='lstm'
-from torch.nn.modules.loss import NLLLoss
-def getDataIterator(opt):
 
-    device = 1 if  torch.cuda.is_available()  else -1
 
-    TEXT = data.Field(lower=True, include_lengths=True, batch_first=True)
-    LABEL = data.Field(sequential=False)
 
-    train, test = datasets.IMDB.splits(TEXT, LABEL)
-
-    TEXT.build_vocab(train, vectors=GloVe(name='6B', dim=300))
-    LABEL.build_vocab(train)    
-    # print vocab information
-    print('len(TEXT.vocab)', len(TEXT.vocab))
-    print('TEXT.vocab.vectors.size()', TEXT.vocab.vectors.size())
-    
-    # make iterator for splits
-    #train_iter, test_iter = data.BucketIterator.splits((train, test), batch_size=3, device=0)
-    train_iter, test_iter = data.BucketIterator.splits((train, test), batch_size=opt.batch_size,device=device)
-    # print batch information
-#    batch = next(iter(train_iter))
-#    print(batch.text)
-#    print(batch.label)
-    
-    opt.label_size= len(LABEL.vocab)
-    
-    opt.vocab_size = len(TEXT.vocab)
-    opt.embedding_dim= TEXT.vocab.vectors.size()[1]
-    opt.embeddings = TEXT.vocab.vectors
-    return train_iter, test_iter
-
-opt.grad_clip=0.001
-train_iter, test_iter = getDataIterator(opt)
+train_iter, test_iter = utils.loadData(opt)
 model=models.setup(opt)
 model.train()
 optimizer = optim.Adam(model.parameters(), lr=opt.learning_rate)
 optimizer.zero_grad()
 loss_fun = NLLLoss()
+
 for batch in train_iter.__iter__():
 
-#    
-#    batch = next(iter(train_iter))
 
     predicted = model(batch.text[0])
 
--- utils.py
+++ utils.py
@@ -1,7 +1,38 @@
 # -*- coding: utf-8 -*-
+import torch
+import torch.nn.functional as F
+from torchtext import data
+from torchtext import datasets
+from torchtext.vocab import Vectors, GloVe, CharNGram, FastText
+
 
 def clip_gradient(optimizer, grad_clip):
     for group in optimizer.param_groups:
         for param in group['params']:       
             if param.grad is not None and param.requires_grad:
                 param.grad.data.clamp_(-grad_clip, grad_clip)
+
+
+def loadData(opt):
+
+    device = 1 if  torch.cuda.is_available()  else -1
+
+    TEXT = data.Field(lower=True, include_lengths=True, batch_first=True)
+    LABEL = data.Field(sequential=False)
+
+    train, test = datasets.IMDB.splits(TEXT, LABEL)
+
+    TEXT.build_vocab(train, vectors=GloVe(name='6B', dim=300))
+    LABEL.build_vocab(train)    
+    # print vocab information
+    print('len(TEXT.vocab)', len(TEXT.vocab))
+    print('TEXT.vocab.vectors.size()', TEXT.vocab.vectors.size())
+
+    train_iter, test_iter = data.BucketIterator.splits((train, test), batch_size=opt.batch_size,device=device)
+
+    opt.label_size= len(LABEL.vocab)    
+    opt.vocab_size = len(TEXT.vocab)
+    opt.embedding_dim= TEXT.vocab.vectors.size()[1]
+    opt.embeddings = TEXT.vocab.vectors
+    
+    return train_iter, test_iter
