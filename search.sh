7a1089a505d7a9e96229bea77c6ef572b143180b
wabywang <wabywang@tencent.com>
2018-04-05 14:00:17 +0800

    multi_process_with_shell

---
+++ search.sh
@@ -0,0 +1,11 @@
+echo "use gpu with multiple processes";
+for((i=0;i<=7;i++))
+do
+    {
+     echo "use gpu" +$i ; 
+     echo CUDA_VISIBLE_DEVICES=$i python parameter_search.py -gpu $i; 
+     CUDA_VISIBLE_DEVICES=$i python multi_search.py -gpu $i; 
+     
+     }&
+done
+wait
